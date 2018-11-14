127b940ebbdb9716edeba34ae048d5037ada17cb
Sotheanith Sok <thetheanith@gmail.com>
2018-11-13 21:06:46 -0800

    complete Assignment 10 for javascript

--- Assignment-10/Assn10.js
+++ Assignment-10/Assn10.js
@@ -1,32 +1,73 @@
-console.log("i. Floating point number converter.\n");
-let pi = 3.1;
-console.log(pi+" -> "+convertToFloat(pi))
+const fs = require('fs');
+main()
 
-// console.log("\nii. Floating point number enumeration.\n");
-// let fp = 0.0;
-// let i = 0;
-// while (fp < 1.4E-44) {
-//     console.log( ++i << "th number: " << (fp = nextFloat(fp)));
-// }
+function main(){
+    console.log("i. Floating point number converter.");
+    let pi = 3.14159265358979;
+    console.log(pi + " -> " + convertToFloat(pi))
 
+    console.log("\nii. Floating point number enumeration.");
+	let fp = 0.0;
+	let i = 0;
+	while (fp < 1.4E-44)
+	{
+		console.log(++i + "th number: " + (fp = nextFloat(fp)));
+	}
 
+    console.log("\niii. Floating point number counting");
+	let posFPs = countBetween(0.0, Number.MAX_VALUE);
+	let zeroOneFPs = countBetween(0.0, 1.0);
+	console.log("Number of positive floating point numbers: " + posFPs);
+	console.log("Number of floating point numbers between 0 and 1: " + zeroOneFPs);
+	console.log("Proportion (# of 0~1) / (# of positive): " + ((zeroOneFPs/posFPs) * 100.0) + "%");
 
-
-
-
-
+    if (process.argv.length != 3)
+	{
+		console.error("\niv. Pass the data file name");
+		return 0;
+	}
+    console.log("\niv. Floating point number distribution");
+    let lower = 0.0, upper, interval = 100.0;
+	for (let i = 0; i < 100; i++)
+	{
+        upper = lower + interval;
+        fs.appendFile(process.argv[2],countBetween(lower, upper)+'\n', (err)=>{
+            if(err){
+                console.error(err);
+            }
+        })
+		lower = upper;
+	}
+	console.log("The output file is ready. Execute \"Python3 Assn9.py " + process.argv[2] + "\"");
+}
 
 function convertToFloat(number) {
-	let sign = (number ) >> 31;
-	let exp = (number &7f800000,16)) >> 23;
-	let mantissa = (number);
-    return "(" + sign + ", " + exp + ", " + mantissa + ")";
+    let view = new DataView(new ArrayBuffer(4));
+    view.setFloat32(0, number);
+    let binary=view.getUint32(0).toString(2);
+    while(binary.length<32){
+        binary="0"+binary;
+    }
+    let sign = parseInt(binary.substring(0,1),2);
+    let exp =  parseInt(binary.substring(1,9),2);
+    let mantissa = parseInt(binary.substring(9),2);
+    return "(" + sign + ", " +exp + ", " + mantissa + ")";
+
 }
 
 function nextFloat(number) {
-
+    let view = new DataView(new ArrayBuffer(4));
+    view.setFloat32(0, number);
+    let binary=(view.getUint32(0)+1);
+    view.setUint32(0,binary);
+    return view.getFloat32(0);    
 }
 
 function countBetween(lower, upper) {
-
+    let view = new DataView(new ArrayBuffer(4));
+    view.setFloat32(0, lower);
+    let l=view.getUint32(0);
+    view.setFloat32(0,upper);
+    let u = view.getUint32(0);
+    return u - l;
 }
